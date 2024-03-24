26b681e8bf71697d46756b51b0def8926308a763
nmktad <nmktadesse@gmail.com>
2024-03-25 01:28:52 +0300

    fix: remove redirect to home

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,9 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/home", request.url));
-}
+export function middleware(request: NextRequest) {}
 
 // See "Matching Paths" below to learn more
 export const config = {
