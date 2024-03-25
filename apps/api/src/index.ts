83ce47b686e7a78eaca7aea8186aa66f5fa135e4
nmktad <nmktadesse@gmail.com>
2024-03-25 16:48:03 +0300

    feat: updated api server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,21 +1,30 @@
-import express from "express";
-import morgan from "morgan";
+import express, { Express } from "express";
 import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import logger from "@wisecare/logging";
+import { pinoHttp } from "@wisecare/logging/middleware";
 
-const app = express();
+const app: Express = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  pinoHttp({
+    logger,
+  }),
+);
 
-app.use(morgan("dev"));
 app.use(cors());
+app.use(helmet());
+app.use(compression());
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.json({ message: "Hello World" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on https://localhost:${PORT}`);
 });
