8989f61bcbee85f56f10966fddc7592c073e2986
nmktad <nmktadesse@gmail.com>
2024-04-07 02:35:25 +0300

    refactor: remove unused service, rename

---
+++ apps/api/src/app.e2e.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer()).get('/health').expect('OK');
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
