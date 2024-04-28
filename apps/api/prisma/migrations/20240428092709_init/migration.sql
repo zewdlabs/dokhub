da36f5361696ed57383edb0bb75261004d5638b6
nmktad <nmktadesse@gmail.com>
2024-04-28 12:41:10 +0300

    feat: modify org to be workspace and schema change

--- apps/api/prisma/migrations/20240428092709_init/migration.sql
+++ apps/api/prisma/migrations/20240428092709_init/migration.sql
@@ -2,27 +2,30 @@
 CREATE TYPE "InvitationStatus" AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED');
 
 -- CreateEnum
-CREATE TYPE "SubscriptionType" AS ENUM ('PRO', 'ENTERPRISE');
+CREATE TYPE "SubscriptionPlan" AS ENUM ('PRO', 'ENTERPRISE');
 
 -- CreateEnum
-CREATE TYPE "Role" AS ENUM ('SUADMIN', 'CREATOR', 'ADMIN', 'USER');
+CREATE TYPE "RoleType" AS ENUM ('SUADMIN', 'CREATOR', 'ADMIN', 'MEMBER');
 
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
-    "firstName" TEXT NOT NULL,
-    "lastName" TEXT NOT NULL,
+    "prefix" TEXT,
+    "username" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "phone" TEXT,
     "bio" TEXT,
+    "refreshToken" TEXT,
     "occupation" TEXT,
     "specialty" TEXT,
     "yearsOfExperience" INTEGER,
     "medicalLicenseNumber" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "role" "Role" NOT NULL DEFAULT 'USER',
+    "followedByCount" INTEGER NOT NULL DEFAULT 0,
+    "followingCount" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
@@ -37,24 +40,12 @@ CREATE TABLE "follows" (
 );
 
 -- CreateTable
-CREATE TABLE "sessions" (
-    "id" TEXT NOT NULL,
-    "token" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "expiresAt" TIMESTAMP(3) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "email_verifications" (
     "id" TEXT NOT NULL,
     "code" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
-    "expiresAt" TIMESTAMP(3) NOT NULL DEFAULT now() + interval '3 day',
+    "expiresAt" TIMESTAMP(3) NOT NULL DEFAULT now() + interval '1 day',
 
     CONSTRAINT "email_verifications_pkey" PRIMARY KEY ("id")
 );
@@ -73,9 +64,9 @@ CREATE TABLE "password_resets" (
 -- CreateTable
 CREATE TABLE "subscriptions" (
     "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "expiresAt" TIMESTAMP(3) NOT NULL DEFAULT now() + interval '1 month',
-    "subscriptionType" "SubscriptionType" NOT NULL DEFAULT 'PRO',
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "workspaceId" TEXT NOT NULL,
+    "subscriptionType" "SubscriptionPlan" NOT NULL DEFAULT 'PRO',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -91,7 +82,10 @@ CREATE TABLE "posts" (
     "published" BOOLEAN NOT NULL DEFAULT false,
     "reportedAmount" INTEGER NOT NULL DEFAULT 0,
     "replyToPostId" TEXT,
+    "repliesCount" INTEGER NOT NULL DEFAULT 0,
+    "postLikeCount" INTEGER NOT NULL DEFAULT 0,
     "authorId" TEXT NOT NULL,
+    "workspaceId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -122,7 +116,7 @@ CREATE TABLE "tags" (
 -- CreateTable
 CREATE TABLE "social_links" (
     "id" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
+    "socialUserName" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -131,24 +125,23 @@ CREATE TABLE "social_links" (
 );
 
 -- CreateTable
-CREATE TABLE "organizations" (
+CREATE TABLE "workspaces" (
     "id" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "location" TEXT NOT NULL,
-    "institutionLicenseNumber" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "workspaces_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "invitations" (
     "id" TEXT NOT NULL,
-    "organizationId" TEXT NOT NULL,
+    "workspaceId" TEXT NOT NULL,
     "expirationDate" TIMESTAMP(3) NOT NULL DEFAULT now() + interval '7 days',
     "email" TEXT NOT NULL,
-    "role" "Role" NOT NULL,
+    "role" "RoleType" NOT NULL DEFAULT 'MEMBER',
     "userId" TEXT NOT NULL,
     "status" "InvitationStatus" NOT NULL DEFAULT 'PENDING',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -158,31 +151,38 @@ CREATE TABLE "invitations" (
 );
 
 -- CreateTable
-CREATE TABLE "_PostToTag" (
+CREATE TABLE "roles" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "workspaceId" TEXT NOT NULL,
+    "role" "RoleType" NOT NULL DEFAULT 'MEMBER',
+
+    CONSTRAINT "roles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_UserToWorkspace" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL
 );
 
 -- CreateTable
-CREATE TABLE "_OrganizationToUser" (
+CREATE TABLE "_PostToTag" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
-CREATE INDEX "users_email_idx" ON "users"("email");
+CREATE INDEX "unique_user" ON "users"("email", "username");
 
 -- CreateIndex
 CREATE INDEX "unique_follows" ON "follows"("followedById", "followingId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "sessions_token_key" ON "sessions"("token");
-
--- CreateIndex
-CREATE INDEX "unique_session" ON "sessions"("userId", "token");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "email_verifications_email_key" ON "email_verifications"("email");
@@ -206,7 +206,7 @@ CREATE UNIQUE INDEX "password_resets_userId_key" ON "password_resets"("userId");
 CREATE INDEX "unique_password_reset" ON "password_resets"("userId", "token", "email");
 
 -- CreateIndex
-CREATE INDEX "subscriptions_userId_idx" ON "subscriptions"("userId");
+CREATE INDEX "subscriptions_workspaceId_idx" ON "subscriptions"("workspaceId");
 
 -- CreateIndex
 CREATE INDEX "posts_authorId_idx" ON "posts"("authorId");
@@ -221,25 +221,28 @@ CREATE UNIQUE INDEX "tags_name_key" ON "tags"("name");
 CREATE INDEX "tags_name_idx" ON "tags"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "social_links_username_key" ON "social_links"("username");
+CREATE INDEX "social_links_userId_idx" ON "social_links"("userId");
 
 -- CreateIndex
-CREATE INDEX "unique_social_link" ON "social_links"("userId", "username");
+CREATE UNIQUE INDEX "workspaces_slug_key" ON "workspaces"("slug");
 
 -- CreateIndex
-CREATE INDEX "unique_invitation" ON "invitations"("organizationId", "userId");
+CREATE INDEX "workspaces_slug_idx" ON "workspaces"("slug");
+
+-- CreateIndex
+CREATE INDEX "unique_invitation" ON "invitations"("workspaceId", "userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_UserToWorkspace_AB_unique" ON "_UserToWorkspace"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_UserToWorkspace_B_index" ON "_UserToWorkspace"("B");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "_PostToTag_AB_unique" ON "_PostToTag"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_PostToTag_B_index" ON "_PostToTag"("B");
-
--- CreateIndex
-CREATE UNIQUE INDEX "_OrganizationToUser_AB_unique" ON "_OrganizationToUser"("A", "B");
-
--- CreateIndex
-CREATE INDEX "_OrganizationToUser_B_index" ON "_OrganizationToUser"("B");
 
 -- AddForeignKey
 ALTER TABLE "follows" ADD CONSTRAINT "follows_followedById_fkey" FOREIGN KEY ("followedById") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -248,22 +251,22 @@ ALTER TABLE "follows" ADD CONSTRAINT "follows_followedById_fkey" FOREIGN KEY ("f
 ALTER TABLE "follows" ADD CONSTRAINT "follows_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "email_verifications" ADD CONSTRAINT "email_verifications_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "password_resets" ADD CONSTRAINT "password_resets_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "posts" ADD CONSTRAINT "posts_replyToPostId_fkey" FOREIGN KEY ("replyToPostId") REFERENCES "posts"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "posts" ADD CONSTRAINT "posts_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "post_likes" ADD CONSTRAINT "post_likes_postId_fkey" FOREIGN KEY ("postId") REFERENCES "posts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -275,19 +278,25 @@ ALTER TABLE "post_likes" ADD CONSTRAINT "post_likes_userId_fkey" FOREIGN KEY ("u
 ALTER TABLE "social_links" ADD CONSTRAINT "social_links_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "invitations" ADD CONSTRAINT "invitations_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "invitations" ADD CONSTRAINT "invitations_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "invitations" ADD CONSTRAINT "invitations_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "roles" ADD CONSTRAINT "roles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "roles" ADD CONSTRAINT "roles_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserToWorkspace" ADD CONSTRAINT "_UserToWorkspace_A_fkey" FOREIGN KEY ("A") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserToWorkspace" ADD CONSTRAINT "_UserToWorkspace_B_fkey" FOREIGN KEY ("B") REFERENCES "workspaces"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_PostToTag" ADD CONSTRAINT "_PostToTag_A_fkey" FOREIGN KEY ("A") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_PostToTag" ADD CONSTRAINT "_PostToTag_B_fkey" FOREIGN KEY ("B") REFERENCES "tags"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_OrganizationToUser" ADD CONSTRAINT "_OrganizationToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_OrganizationToUser" ADD CONSTRAINT "_OrganizationToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,145 +1,135 @@
-import { PrismaClient, Role } from '@prisma/client';
+import { PrismaClient, RoleType } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
-import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  // const initialPassword = 'password123';
-  // const hashedPassword = await bcrypt.hash(initialPassword, 10);
-  const roles = [Role.USER, Role.ADMIN, Role.CREATOR, Role.SUADMIN];
-  const passwords = [
-    'password123',
-    'password234',
-    'password345',
-    'password456',
-    'password567',
-    'password678',
-  ];
-  let count = 0;
-  const users = Array.from({ length: 5 }, async () => {
-    // Generate a new hashed password for each user
-
-    const newPassword = passwords[count];
-    const newHashedPassword = await bcrypt.hash(newPassword, 10);
-    const randomRole = roles[Math.floor(Math.random() * roles.length)];
-    count++;
-    return {
-      fullName: faker.name.fullName(),
-      email: faker.internet.email(),
-      password: newHashedPassword,
-      phone: faker.phone.number(),
-      bio: faker.lorem.sentence(),
-      refreshToken: null,
-      occupation: faker.name.jobTitle(),
-      specialty: faker.lorem.word(),
-      yearsOfExperience: faker.datatype.number({ min: 0, max: 20 }),
-      medicalLicenseNumber: faker.random.alphaNumeric(9), // Assuming it's a 9-character alphanumeric string
-      role: randomRole,
-    };
-  });
-
-  // Add the generated users to the database
-  await prisma.user.createMany({
-    data: await Promise.all(users),
-  });
-  await prisma.user.createMany({
-    data: [
-      {
-        fullName: 'Aiden Johnson',
-        prefix: 'Dr.',
-        email: 'aiden.johnson@example.com',
-        password: await bcrypt.hash('aiden123', 10),
-        phone: '555-123-4567',
-        bio: 'Experienced surgeon specializing in orthopedics.',
-        refreshToken: null,
-        occupation: 'Surgeon',
-        specialty: 'Orthopedics',
-        yearsOfExperience: 15,
-        medicalLicenseNumber: 'GHI789JKL',
-        role: Role.USER,
-      },
-      {
-        fullName: 'Sophia Patel',
-        prefix: null,
-        email: 'sophia.patel@example.com',
-        password: await bcrypt.hash('sophia123', 10),
-        phone: '555-987-6543',
-        bio: "Passionate pediatrician dedicated to children's health.",
-        refreshToken: null,
-        occupation: 'Pediatrician',
-        specialty: 'Pediatrics',
-        yearsOfExperience: 8,
-        medicalLicenseNumber: 'MNO012PQR',
-        role: Role.ADMIN,
-      },
-      {
-        fullName: 'Elijah Chang',
-        prefix: 'Dr.',
-        email: 'elijah.chang@example.com',
-        password: await bcrypt.hash('elijah123', 10),
-        phone: '555-789-0123',
-        bio: 'Skilled cardiologist with a focus on preventive care.',
-        refreshToken: null,
-        occupation: 'Cardiologist',
-        specialty: 'Cardiology',
-        yearsOfExperience: 12,
-        medicalLicenseNumber: 'STU345VWX',
-        role: Role.SUADMIN,
-      },
-      {
-        fullName: 'Isabella Garcia',
-        prefix: null,
-        email: 'isabella.garcia@example.com',
-        password: await bcrypt.hash('isabella123', 10),
-        phone: '555-234-5678',
-        bio: 'Compassionate nurse committed to patient care.',
-        refreshToken: null,
-        occupation: 'Nurse',
-        specialty: null,
-        yearsOfExperience: 6,
-        medicalLicenseNumber: null,
-        role: Role.CREATOR,
-      },
-      // Add more seed objects as needed
-    ],
-  });
   const user1 = await prisma.user.create({
     data: {
-      fullName: 'User 1',
-      email: 'user1@example.com',
-      password: 'password1',
-      role: Role.ADMIN,
+      fullName: 'Aiden Johnson',
+      username: 'aidenjohnson',
+      prefix: 'Dr.',
+      email: 'aiden.johnson@example.com',
+      password: await bcrypt.hash('aiden123', 10),
+      phone: '555-123-4567',
+      bio: 'Experienced surgeon specializing in orthopedics.',
+      refreshToken: null,
+      occupation: 'Surgeon',
+      specialty: 'Orthopedics',
+      yearsOfExperience: 15,
+      medicalLicenseNumber: 'GHI789JKL',
+    },
+  });
+
+  const user1Workspace = await prisma.workspace.create({
+    data: {
+      name: 'Personal Workspace',
+      slug: 'aidenjohnson',
+      roles: {
+        create: { user: { connect: { id: user1.id } }, role: RoleType.CREATOR },
+      },
+      members: {
+        connect: [user1],
+      },
     },
   });
 
   const user2 = await prisma.user.create({
     data: {
-      fullName: 'User 2',
-      email: 'user2@example.com',
-      password: await bcrypt.hash('password2', 10),
-      role: Role.CREATOR,
+      fullName: 'Sophia Patel',
+      username: 'sophiapatel',
+      prefix: null,
+      email: 'sophia.patel@example.com',
+      password: await bcrypt.hash('sophia123', 10),
+      phone: '555-987-6543',
+      bio: "Passionate pediatrician dedicated to children's health.",
+      refreshToken: null,
+      occupation: 'Pediatrician',
+      specialty: 'Pediatrics',
+      yearsOfExperience: 8,
+      medicalLicenseNumber: 'MNO012PQR',
     },
   });
 
-  await prisma.user.create({
+  await prisma.workspace.create({
     data: {
-      fullName: 'User 3',
-      email: 'user3@example.com',
-      password: await bcrypt.hash('password3', 10),
+      name: 'Personal Workspace',
+      slug: 'sophiapatel',
+      roles: {
+        create: { user: { connect: { id: user2.id } }, role: RoleType.CREATOR },
+      },
     },
   });
 
-  // Create organization with members
-  await prisma.organization.create({
+  const user3 = await prisma.user.create({
     data: {
-      name: 'Organization 1',
-      slug: 'org1',
-      location: 'Location 1',
+      fullName: 'Elijah Chang',
+      username: 'elijahchang',
+      prefix: 'Dr.',
+      email: 'elijah.chang@example.com',
+      password: await bcrypt.hash('elijah123', 10),
+      phone: '555-789-0123',
+      bio: 'Skilled cardiologist with a focus on preventive care.',
+      refreshToken: null,
+      occupation: 'Cardiologist',
+      specialty: 'Cardiology',
+      yearsOfExperience: 12,
+      medicalLicenseNumber: 'STU345VWX',
+    },
+  });
+
+  await prisma.workspace.create({
+    data: {
+      name: 'Personal Workspace',
+      slug: 'elijahchang',
+      roles: {
+        create: { user: { connect: { id: user3.id } }, role: RoleType.CREATOR },
+      },
+    },
+  });
+
+  const user4 = await prisma.user.create({
+    data: {
+      username: 'isabellagarcia',
+      fullName: 'Isabella Garcia',
+      prefix: null,
+      email: 'isabella.garcia@example.com',
+      password: await bcrypt.hash('isabella123', 10),
+      phone: '555-234-5678',
+      bio: 'Compassionate nurse committed to patient care.',
+      refreshToken: null,
+      occupation: 'Nurse',
+      specialty: null,
+      yearsOfExperience: 6,
+      medicalLicenseNumber: null,
+    },
+  });
+
+  await prisma.workspace.create({
+    data: {
+      name: 'Personal Workspace',
+      slug: 'isabellagarcia',
+      roles: {
+        create: { user: { connect: { id: user4.id } }, role: RoleType.CREATOR },
+      },
+    },
+  });
+
+  const workspace1 = await prisma.workspace.create({
+    data: {
+      name: "Saint Paul's Millennium Medical College",
+      slug: 'spmmc',
       members: {
         connect: [
           { id: user1.id }, // Connect user1 to the organization
           { id: user2.id }, // Connect user2 to the organization
+        ],
+      },
+
+      roles: {
+        create: [
+          { user: { connect: { id: user1.id } }, role: RoleType.CREATOR },
+          { user: { connect: { id: user2.id } }, role: RoleType.MEMBER },
         ],
       },
     },
@@ -151,7 +141,8 @@ async function main() {
       title: 'Post 1',
       content: 'Content of Post 1',
       published: true,
-      authorId: user1.id, // User 1 creates this post
+      authorId: user2.id, // User 1 creates this post
+      workspaceId: workspace1.id,
     },
   });
 
@@ -161,6 +152,7 @@ async function main() {
       content: 'Content of Post 2',
       published: true,
       authorId: user2.id, // User 2 creates this post
+      workspaceId: user1Workspace.id,
     },
   });
 }
