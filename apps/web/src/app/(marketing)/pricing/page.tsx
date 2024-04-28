3364564827a7f42e8c3476d4e0b6db7564c0b69c
nmktad <nmktadesse@gmail.com>
2024-04-28 12:16:48 +0300

    feat: make pricing page

---
+++ apps/web/src/app/(marketing)/pricing/page.tsx
@@ -0,0 +1,74 @@
+export default function PricingPage() {
+  const [isYearly, setIsYearly] = useState(false);
+  const togglePricingPeriod = (value: string) =>
+    setIsYearly(parseInt(value) === 1);
+
+  const plans = [
+    {
+      title: "Basic",
+      monthlyPrice: 10,
+      yearlyPrice: 100,
+      description: "Essential features you need to get started",
+      features: [
+        "Example Feature Number 1",
+        "Example Feature Number 2",
+        "Example Feature Number 3",
+      ],
+      actionLabel: "Get Started",
+    },
+    {
+      title: "Pro",
+      monthlyPrice: 25,
+      yearlyPrice: 250,
+      description: "Perfect for owners of small & medium businessess",
+      features: [
+        "Example Feature Number 1",
+        "Example Feature Number 2",
+        "Example Feature Number 3",
+      ],
+      actionLabel: "Get Started",
+      popular: true,
+    },
+    {
+      title: "Enterprise",
+      price: "Custom",
+      description: "Dedicated support and infrastructure to fit your needs",
+      features: [
+        "Example Feature Number 1",
+        "Example Feature Number 2",
+        "Example Feature Number 3",
+        "Super Exclusive Feature",
+      ],
+      actionLabel: "Contact Sales",
+      exclusive: true,
+    },
+  ];
+  return (
+    <div className="py-8">
+      <section className="text-center">
+        <h2 className="text-3xl font-bold">Pricing Plans</h2>
+        <p className="text-xl pt-1">Choose the one that's suitable for you</p>
+        <br />
+      </section>
+      <PricingSwitch onSwitch={togglePricingPeriod} />
+      <section className="flex flex-col sm:flex-row sm:flex-wrap justify-center gap-8 mt-8">
+        {plans.map((plan) => {
+          return <PricingCard key={plan.title} {...plan} isYearly={isYearly} />;
+        })}
+      </section>
+    </div>
+  );
+}
+
+const PricingSwitch = ({ onSwitch }: PricingSwitchProps) => (
+  <Tabs defaultValue="0" className="w-40 mx-auto" onValueChange={onSwitch}>
+    <TabsList className="py-6 px-2">
+      <TabsTrigger value="0" className="text-base">
+        Monthly
+      </TabsTrigger>
+      <TabsTrigger value="1" className="text-base">
+        Yearly
+      </TabsTrigger>
+    </TabsList>
+  </Tabs>
+);
