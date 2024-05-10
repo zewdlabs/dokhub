1fe6b18cbaec70c78203462559d0e8c5f425a75f
nmktad <nmktadesse@gmail.com>
2024-05-10 13:52:02 +0300

    nochange: add logs

--- apps/web/src/auth.ts
+++ apps/web/src/auth.ts
@@ -16,50 +16,61 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       },
 
       async authorize(credentials) {
-        if (!credentials.email || !credentials.password) return null;
+        if (credentials.email && credentials.password) {
+          const res = await fetch(`${process.env.BACKEND_URL}/auth/signin`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(credentials),
+          });
 
-        const res = await fetch(`${process.env.BACKEND_URL}/auth/signin`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(credentials),
-        });
-
-        if (res.ok) {
-          return await res.json();
+          if (res.ok) {
+            return await res.json();
+          }
         }
+
+        return null;
       },
     }),
   ],
   callbacks: {
     async signIn({ user, account, profile, credentials, email }) {
-      // console.log(">>> signIn callback called");
-      //
-      // console.log({ user });
-      // profile && console.log({ profile });
-      // account && console.log({ account });
-      // credentials && console.log({ credentials });
-      // email && console.log({ email });
+      if (account?.provider === "google") {
+        // Perform nest backend call to check if the user is in the database
+        // if the user exists in the database, return the user object
+        // if not, create the user on the backend with the information from the google account and return the user object
+      }
+
+      if (credentials) {
+      }
+
+      console.log(">>> signIn callback called");
+
+      console.log({ user });
+      profile && console.log({ profile });
+      account && console.log({ account });
+      credentials && console.log({ credentials });
+      email && console.log({ email });
 
       return true;
     },
     async jwt({ token, user, account, profile, trigger }) {
-      // console.log("JWT callback called");
-      //
-      // console.log({ token });
-      // console.log({ user });
-      // console.log({ trigger });
-      // account && console.log({ account });
-      // profile && console.log({ profile });
+      console.log("JWT callback called");
+
+      console.log({ token });
+      console.log({ user });
+      console.log({ trigger });
+      account && console.log({ account });
+      profile && console.log({ profile });
 
       return token;
     },
 
     async session({ session, token, user, trigger }) {
-      // console.log(">>> Session callback called");
-      // console.log({ session });
-      // console.log({ token });
-      // console.log({ user });
-      // console.log({ trigger });
+      console.log(">>> Session callback called");
+      console.log({ session });
+      console.log({ token });
+      console.log({ user });
+      console.log({ trigger });
 
       return session;
     },
