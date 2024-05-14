13e1d1d6430f298c8789de67c80ca447a03d035e
mahi16tz <mahletalmi@gmail.com>
2024-05-14 20:09:37 +0300

    the fileupload and test done

--- apps/api/src/users/user.module.ts
+++ apps/api/src/users/user.module.ts
@@ -2,11 +2,17 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaModule } from '@/prisma/prisma.module';
 import { UserController } from './user.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [PrismaModule],
   exports: [UserService],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class UserModule {}
